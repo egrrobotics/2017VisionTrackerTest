977e6f42edc035c00767efcd9ec1e88c9d1c745b
anton <anton@LAPTOP-1VS555VU>
2017-12-21 19:16:21 -0500

    fixed streaming (empty source issue), and starting filtering HSV

--- 2017VisionTrackerTest/src/org/usfirst/frc/team5980/robot/Robot.java
+++ 2017VisionTrackerTest/src/org/usfirst/frc/team5980/robot/Robot.java
@@ -40,8 +40,9 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putData("Auto mode", chooser);
 		SmartDashboard.putString("Hello ", "World");
 		System.out.println("Please work");
+		
 		camera = new Cameras();
-		camera.startCamera();
+                camera.startCamera();
 	}
 
 	/**
@@ -100,6 +101,7 @@ public class Robot extends IterativeRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
+	        
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
 	}
--- 2017VisionTrackerTest/src/org/usfirst/frc/team5980/robot/subsystems/Cameras.java
+++ 2017VisionTrackerTest/src/org/usfirst/frc/team5980/robot/subsystems/Cameras.java
@@ -1,7 +1,11 @@
 package org.usfirst.frc.team5980.robot.subsystems;
 
+import java.util.Date;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
+import org.opencv.imgproc.*;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -15,7 +19,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Cameras extends Subsystem implements Runnable {
     Thread tracking;
-    Mat source, mask, hsv, hierarchy;
+    Mat source, mask, hsv, hierarchy, gray;
     Scalar lowerHSV, upperHSV;
     UsbCamera camera;
     Object visionLock = new Object();
@@ -24,65 +28,91 @@ public class Cameras extends Subsystem implements Runnable {
     double targetY = Double.NaN;
     CvSink currentSink;
     UsbCamera currentCam;
+    CvSource outputStream;
+    int localWidth = 320;
+    int localHeight = 240;
     
-    public Cameras () {
+    public Cameras() {
         source = new Mat();
         mask = new Mat();
         hsv = new Mat();
         hierarchy = new Mat();
-        lowerHSV = new Scalar(55,100,100);//75
-        upperHSV = new Scalar(100,255,255);//160
+        gray = new Mat();
+        lowerHSV = new Scalar(55, 100, 100);// 75
+        upperHSV = new Scalar(100, 255, 255);// 160
         tracking = new Thread(this);
+        
+        /*
         camera = new UsbCamera("front", 0);
         currentCam = camera;
         CameraServer.getInstance().addCamera(currentCam);
         currentSink = CameraServer.getInstance().getVideo(currentCam);
+        */
+        
+        camera = CameraServer.getInstance().startAutomaticCapture();
+        //camera.setResolution(localWidth, localHeight);
+        currentSink = CameraServer.getInstance().getVideo();
+        outputStream = CameraServer.getInstance().putVideo("Vision", localWidth, localHeight);// creates a
+        
     }
-    
+
     public void trackingOn(boolean on) {
-        synchronized(visionLock) {
-                trackingOn = on;
-                if(on == false) {
-                        targetX = Double.NaN;
-                        targetY = Double.NaN;
-                }
+        synchronized (visionLock) {
+            trackingOn = on;
+            if (on == false) {
+                targetX = Double.NaN;
+                targetY = Double.NaN;
+            }
         }
     }
-    
+
     public boolean isTrackingOn() {
-        synchronized(visionLock) {
-                return trackingOn;
+        synchronized (visionLock) {
+            return trackingOn;
         }
     }
 
     public void startCamera() {
         tracking.start();
     }
-    
+
     public void run() {
-        int localWidth = 320;
-        int localHeight = 240;
-        //camera.setResolution(localWidth, localHeight);//sets resolution (7mb/s bandwidth limit!)
-        camera.setPixelFormat(PixelFormat.kYUYV);//sets the pixel format for fast camera switching (C920)
-        camera.setFPS(20);//sets fps
+        
+        camera.setResolution(localWidth, localHeight);// sets resolution (7mb/s bandwidth limit!)
+        
+        camera.setPixelFormat(PixelFormat.kYUYV);// sets the pixel format for fast camera switching (C920)
+        camera.setFPS(20);// sets fps
         camera.setBrightness(20);
+        camera.setExposureManual(1);
         
-        CvSource outputStream = CameraServer.getInstance().putVideo("Vision",  localWidth, localHeight);//creates a stream to the SmartDashboard
-        while(true) {
-            synchronized(visionLock) {
-                currentSink.grabFrame(source);
+        
+                                                                                                       // SmartDashboard
+        while (true) {
+            // synchronized(visionLock) {
+            //System.out.println(source);
+            //long frameTime = currentSink.grabFrame(source, 2.0);
+            //camera.setResolution(localWidth, localHeight);
+            currentSink.grabFrame(source);
+            
+            if(source.empty()) {
+                System.out.println("is empty");
+            } else {
+                Imgproc.cvtColor(source, hsv, Imgproc.COLOR_BGR2HSV);
+                Core.inRange(hsv, lowerHSV, upperHSV, mask);
+                outputStream.putFrame(mask);
             }
-            outputStream.putFrame(source);
+             
+            
+            //System.out.println(new Date());
         }
-        
+
     }
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-        
+        // setDefaultCommand(new MySpecialCommand());
+
     }
 }
-
